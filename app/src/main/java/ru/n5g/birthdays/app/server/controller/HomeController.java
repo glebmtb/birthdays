b70a2d1768b8d34845f52fad85c48e7bdbbe96d0
Беляев Глеб <Belyaev.Gleb@cg.ru>
2012-04-13 10:11:46 +0400

    Тестовое сообщение с контролера

--- app/src/main/java/ru/n5g/birthdays/app/server/controller/HomeController.java
+++ app/src/main/java/ru/n5g/birthdays/app/server/controller/HomeController.java
@@ -24,15 +24,15 @@ public class HomeController {
    */
   @RequestMapping(value = "/", method = RequestMethod.GET)
   public String home(Locale locale, Model model) {
-    logger.info("Welcome home! the client locale is "+ locale.toString());
+    logger.info("Welcome home! the client locale is " + locale.toString());
 
     Date date = new Date();
     DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 
     String formattedDate = dateFormat.format(date);
 
-    model.addAttribute("serverTime", formattedDate );
-
+    model.addAttribute("serverTime", formattedDate);
+    model.addAttribute("serverMessage", "Hello World Controller");
     return "home";
   }
 
