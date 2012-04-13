fb497a1b0daa7d2937320830339309fb1cd1c45e
Беляев Глеб <Belyaev.Gleb@cg.ru>
2012-04-13 10:04:28 +0400

    Добавил класс Сервиса

---
+++ app/src/main/java/ru/n5g/birthdays/app/server/service/BirthdaysServiceImpl.java
@@ -0,0 +1,10 @@
+package ru.n5g.birthdays.app.server.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BirthdaysServiceImpl {
+  public String getHelloWorld(){
+    return "Hello World Service";
+  }
+}
