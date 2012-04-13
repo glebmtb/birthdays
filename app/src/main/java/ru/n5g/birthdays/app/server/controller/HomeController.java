417a0c992097394a26efe8b3cdf286ef1d62b803
Беляев Глеб <Belyaev.Gleb@cg.ru>
2012-04-13 10:15:46 +0400

    Прикрутил сервис к контролеру

--- app/src/main/java/ru/n5g/birthdays/app/server/controller/HomeController.java
+++ app/src/main/java/ru/n5g/birthdays/app/server/controller/HomeController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import ru.n5g.birthdays.app.server.service.BirthdaysServiceImpl;
 
 /**
  * Handles requests for the application home page.
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
 
   private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+  private final BirthdaysServiceImpl service = new BirthdaysServiceImpl();
 
   /**
    * Simply selects the home view to render by returning its name.
@@ -32,7 +34,7 @@ public class HomeController {
     String formattedDate = dateFormat.format(date);
 
     model.addAttribute("serverTime", formattedDate);
-    model.addAttribute("serverMessage", "Hello World Controller");
+    model.addAttribute("serverMessage", service.getHelloWorld());
     return "home";
   }
 
