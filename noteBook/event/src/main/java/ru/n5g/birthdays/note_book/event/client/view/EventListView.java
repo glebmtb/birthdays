94c00f0588b99b5333b5e8a2dd41aae78d42c030
Belyaev Gleb <Belyaev.Gleb@cg.ru>
2013-01-17 16:24:22 +0400

    добавил задачи класса

--- noteBook/event/src/main/java/ru/n5g/birthdays/note_book/event/client/view/EventListView.java
+++ noteBook/event/src/main/java/ru/n5g/birthdays/note_book/event/client/view/EventListView.java
@@ -16,15 +16,25 @@ public class EventListView extends LayoutContainer implements EventListPresenter
 
   }
 
+
+  //Сделать поиск по ФИО
+  //Выборка по Поводу
+  //Подумать как сделать недавние события
+
   @Override
   protected void onRender(Element parent, int index) {
     super.onRender(parent, index);
 
+    //Вывести список событий
+    //Сделать три колонки
+    //Дней до события (сортировать по возрастанию по умолчанию) пример "22д. [22 мая 1988 (22г.)]"
+    //Повод "День рождения"
+    //Фио "(Друган) Андреева Катерина Петровна"
   }
 
 
   @Override
   public void onRefresh() {
-
+    //Обновить список событий
   }
 }
